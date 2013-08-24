db21d4d434be77dacad900259118f471f055bbd3
Austin Lopez <ahlopez@ahlopez-K53E.(none)>
2013-08-24 11:46:48 -0400

    index.tpl modifications, assuming a future ng-controller that handles rendering index.tpl for a given page. An empty flask test file was created as well.

---
+++ tests/app_tests.py
@@ -0,0 +1,9 @@
+import os
+import app
+import unittest
+
+# TODO. Think about the testing framwork here. Tests should be 
+# divided as follows:
+# -SQLAlchemy tests
+# -Flask middle logic tests (primarily RESTful API stuff)
+# -Angular js front end tests
