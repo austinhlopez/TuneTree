595b2a5748d61da3ef42a09407e325d6c874161c
Austin Lopez <austinhlopez@gmail.com>
2013-08-26 21:10:50 -0400

    Added a mock require.js file, updated index.html to show it

---
+++ tuneTree/static/js/main.js
@@ -0,0 +1,7 @@
+/* A sample require.js entry. We'll add more when we learn more. */
+require(["lib/angular.min.js"], function(angular) {
+    /* This function is called when lib/angular.min.js is loaded.
+       if angular.js calls define(), then this function is 
+       not fired until angular's dependencies have loaded, and the
+       angular argument will hold the module value for "lib/angular.min.js*/
+}
